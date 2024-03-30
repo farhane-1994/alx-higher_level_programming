3adc18a292e039b9c66e01044497bc5262ee887e
farhane-1994 <133435229+farhane-1994@users.noreply.github.com>
2024-03-30 18:02:05 +0000

    Delete 0x10. Python - Network #0 directory

--- 0x10. Python - Network #0/0-body_size.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# This script takes a URL as input, sends a request to that URL using curl,
-# and displays the size of the response body in bytes
-
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
