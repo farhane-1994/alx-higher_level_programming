1311859ccbb4b4efeecc2f2db3faaf45d8c46c79
farhane-1994 <133435229+farhane-1994@users.noreply.github.com>
2024-03-30 17:58:24 +0000

    Update 0-body_size.sh

--- 0x10. Python - Network #0/0-body_size.sh
+++ 0x10. Python - Network #0/0-body_size.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
+# This script takes a URL as input, sends a request to that URL using curl,
+# and displays the size of the response body in bytes
 
-# Check if a URL is provided as an argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Store the URL provided as an argument
-url=$1
-
-# Send a request to the URL and store the response body in a variable
-response=$(curl -s -w "%{size_download}" -o /dev/null "$url")
-
-# Display the size of the response body in bytes
-echo "Size of the response body: $response bytes"
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
