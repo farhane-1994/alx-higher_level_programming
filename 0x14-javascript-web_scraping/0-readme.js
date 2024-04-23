b3dcef68645124a34470b5be16c297a28a9192ba
farhane-1994 <133435229+farhane-1994@users.noreply.github.com>
2024-04-23 12:29:24 +0100

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,12 @@
+#!/usr/bin/node
 const fs = require('fs');
 
 const filePath = process.argv[2];
+
+if (!filePath) {
+  console.error("Please provide a file path as an argument.");
+  process.exit(1);
+}
 
 fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
